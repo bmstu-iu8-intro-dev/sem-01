ee3447868cb15a52f97696bafca9a7e2c9c046ff
drewxa <a.borodin@bmstu.ru>
2018-04-19 22:40:35 +0300

    added ut

---
+++ test/info.ut.js
@@ -0,0 +1,27 @@
+let assert = require('assert');
+let info = require('../info');
+
+
+function test1(){
+    it('Point (0, 0) in area [(-1, -1), (1, 1)]', function () {
+        assert.equal(info.isPointInArea(0, 0, -1, -1, 1, 1), true);
+    });
+}
+
+function test2(){
+    it('Point (0, 0) not in area [(1, 1), (2, 2)]', function () {
+        assert.equal(info.isPointInArea(0, 0, 1, 1, 2, 2), false);
+    });
+
+    it('Point (1.5, 0) not in area [(1, 1), (2, 2)]', function () {
+        assert.equal(info.isPointInArea(1.5, 0, 1, 1, 2, 2), false);
+    });
+}
+
+
+describe('Test isPointInArea', function() {
+
+    describe('In area', test1);
+    describe('Not in area', test2);
+
+});
